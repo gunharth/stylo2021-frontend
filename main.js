e446fcd2ebaf44189c195b722f35297ac5a31689
Gunharth Randolf <hello@gunharth.io>
2021-03-19 17:36:32 +0100

    dynamic colouring for first slides in sections on fullpage.js afterRender

---
+++ main.js
@@ -0,0 +1,64 @@
+// needs to be build up in slider loop with slugs
+let slideAnchors = [
+  "section1",
+  "section2",
+  "section3",
+  "section4",
+  "section5",
+  "section6",
+  "section7",
+  "section8",
+  "section9",
+  "section10",
+  "footer",
+];
+$(function () {
+  $("#fullpage").fullpage({
+    anchors: slideAnchors,
+    //sectionsColor: ["#000000", "#000000", "#000000", "#000000"],
+    navigation: false,
+    navigationPosition: "right",
+    navigationTooltips: [],
+    slidesNavigation: true,
+    slidesNavPosition: "bottom",
+    controlArrows: true,
+    loopHorizontal: true,
+    lazyLoading: true,
+    onLeave(index, nextIndex, direction) {},
+    afterLoad: function (anchorLink, index) {},
+    afterSlideLoad: function (section, origin, destination, direction) {
+      $("#slide-" + section.anchor)
+        .find("h2")
+        .addClass(destination.anchor);
+      $("#slide-" + section.anchor)
+        .find(".fp-slidesNav ul li a span")
+        .addClass(destination.anchor);
+      $("#slide-" + section.anchor)
+        .find(".fp-controlArrow.fp-prev")
+        .addClass(destination.anchor);
+      $("#slide-" + section.anchor)
+        .find(".fp-controlArrow.fp-next")
+        .addClass(destination.anchor);
+      //console.log(section)
+      //console.log(destination)
+    },
+    afterRender: function() {
+        $('.section').each(function(index) {
+            $(this)
+              .find("h2")
+              .addClass('slide1');
+            $(this)
+              .find(".fp-slidesNav ul li a span")
+              .addClass('slide1');
+            $(this)
+              .find(".fp-controlArrow.fp-prev")
+              .addClass('slide1');
+            $(this)
+              .find(".fp-controlArrow.fp-next")
+              .addClass('slide1');
+            // console.log($(this))
+        });
+        // console.log('render')
+    }
+  });
+});
